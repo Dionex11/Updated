46566db50f3247346910453eff89591d2758b3a0
dionex011 <code.testx11@gmail.com>
2023-03-18 17:40:39 +0530

    Merge remote-tracking branch 'github/master'

--- lib/BPDisplay.dart
+++ lib/BPDisplay.dart
@@ -53,21 +53,24 @@ class  _BpdisplayState extends State<Bpdisplay> {
   Widget build(BuildContext context) {
     string_parser( i){
       String a="";
-      List<int> al=[];
+      List<String> al=[];
       i=String.fromCharCodes(i);
       i=i.toString();
-      for(int d=0;d<13;d++){
+      int l=i.length;
+      for(int d=0;d<l;d++){
         if(i[d]!=","){
           a=a+i[d];
         }
-        else{  print(a);
+        else{
+          al.add(a);
           a="";
-          al.add(int()a);
+          print("list  value ------------");
+          print(al);
 
         }
       }
-
-
+     al.add(a);
+    return al;
 
     }
     return Scaffold(
@@ -76,7 +79,7 @@ class  _BpdisplayState extends State<Bpdisplay> {
         backgroundColor: Colors.blue[900],
         shadowColor: Colors.lightBlue[400],
       ),
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.black,
 
       body:StreamBuilder(
         stream: h.stream,
@@ -92,10 +95,68 @@ class  _BpdisplayState extends State<Bpdisplay> {
               return const Text('Error');
             } else if (snapshot.hasData) {
               var i =snapshot.data;
-              string_parser(i);
-              return Text(
-                  String.fromCharCodes(i),
-                  style: const TextStyle(color: Colors.red, fontSize: 40)
+              List<String> d=string_parser(i);
+              return Card(
+                margin: EdgeInsets.fromLTRB(10, 80, 10, 100),
+                child: Column(children:[Row(
+                  children:[Card(
+                    color: Colors.red,
+                    child: Container(
+                      margin: EdgeInsets.all(10),
+                      color:Colors.yellowAccent,
+                      width: 150.0,
+                      height: 150.0,
+                      child: Center(
+                        child: Text(
+                            d[0],
+                            style: const TextStyle(color: Colors.red, fontSize: 40)),
+                      ),
+                    ),
+                  ),
+
+                  Card(
+                    child: Container(
+                      margin: EdgeInsets.all(10),
+                      color:Colors.yellowAccent,
+                      width: 150.0,
+                      height: 150.0,
+                      child: Center(
+                        child: Text(
+                            d[1],
+                            style: const TextStyle(color: Colors.red, fontSize: 40)),
+                      ),
+                    ),
+                  ),
+                ]),
+                  Row(
+                    children:[ Card(
+                      child: Container(
+                        margin: EdgeInsets.all(10),
+                        color:Colors.yellowAccent,
+                        width: 150.0,
+                        height: 150.0,
+                        child: Center(
+                          child: Text(
+                              d[2],
+                              style: const TextStyle(color: Colors.red, fontSize: 40)),
+                        ),
+                      ),
+                    ),
+
+                  Card(
+                    child: Container(
+                      margin: EdgeInsets.all(10),
+                      color:Colors.yellowAccent,
+                      width: 150.0,
+                      height: 150.0,
+                      child: Center(
+                        child: Text(
+                            d[3],
+                            style: const TextStyle(color: Colors.red, fontSize: 40)),
+                      ),
+                    ),
+                  )]),
+                ]),
               );
             } else {
               return const Text('Empty data');
